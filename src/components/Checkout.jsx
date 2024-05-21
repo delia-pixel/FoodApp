f5f1b2d2f4649652636969452eee9b2c8584ee9e
delia-pixel <tpatriciareinedia@gmail.com>
2024-05-21 17:23:02 +0100

    checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -18,12 +18,29 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/meals", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        items: cartCtx.items,
+        order: customerData,
+      }),
+    });
+  };
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full name" type="text" id="full-name" />
+        <Input label="Full name" type="text" id="name" />
         <Input label="Email Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
